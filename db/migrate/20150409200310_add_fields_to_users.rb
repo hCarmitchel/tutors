a1370420c05b6b1e956cd49ae3b94fd36e21ec6b
Heather Carmitchel <Heather.Carmitchel@cerner.com>
2015-04-10 19:35:24 -0500

    Blob

--- db/migrate/20150409200310_add_fields_to_users.rb
+++ db/migrate/20150409200310_add_fields_to_users.rb
@@ -2,7 +2,7 @@ class AddFieldsToUsers < ActiveRecord::Migration
   def change
     add_column :users, :first_name, :string
     add_column :users, :last_name, :string
-    add_column :users, :subjects, :blob
+    add_column :users, :subjects, :bytea
     add_column :users, :tutor, :boolean, default: 0
     add_column :users, :school, :string
   end
--- db/migrate/20150410024516_add_website_to_users.rb
+++ db/migrate/20150410024516_add_website_to_users.rb
@@ -1,9 +1,9 @@
 class AddWebsiteToUsers < ActiveRecord::Migration
   def change
-    add_column :users, :profile, :blob
+    add_column :users, :profile, :text
     add_column :users, :website, :string
     add_column :users, :phone_number, :string
-    add_column :users, :tutor_types, :blob
+    add_column :users, :tutor_types, :bytea
     add_column :users, :admin, :boolean, default: 0
   end
 end
